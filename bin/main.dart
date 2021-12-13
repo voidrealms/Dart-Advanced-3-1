2964f40d25ade3820fd45c8a0d68c558fb3bff85
Bryan Cairns <bcairns@voidrealms.com>
2021-12-13 12:41:03 -0500

    Update main.dart

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,34 @@
 import 'dart:io';
 import 'dart:convert';
+ 
+void main(List<String> arguments) {
+ 
+  String data = '';
+  for(int i = 0; i < 100; i++) {
+    data = data + 'Hello Vorllld!!!\r\n';
+  }
+ 
+  //Original Data
+  var original = utf8.encode(data);
+  //Compress data
+  var compressed = gzip.encode(original);
+  //Decompress
+  var decompress = gzip.decode(compressed);
+ 
+  print('Original ${original.length} bytes');
+  print('Compressed ${compressed.length} bytes');
+  print('Decompressed ${decompress.length} bytes');
+ 
+  String decoded = utf8.decode(decompress);
+  assert(data ==  decoded);
+ 
+  print(decoded);
+}
+
+/*
+old code
+import 'dart:io';
+import 'dart:convert';
 
 main(List<String> arguments) {
 
@@ -28,3 +57,4 @@ main(List<String> arguments) {
   print(decoded);
 
 }
+*/
